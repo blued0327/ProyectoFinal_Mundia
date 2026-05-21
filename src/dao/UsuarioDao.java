7793e00b826e2bcfabb81a3a45e61e41b2368a3b
jdmm0 <jdmm0@Jose>
2026-05-21 07:46:52 -0600

    DAO con sp

--- src/dao/UsuarioDao.java
+++ src/dao/UsuarioDao.java
@@ -16,8 +16,7 @@ public class UsuarioDao {
         //query -- se cambia por los procedures sp--tengan cuidado con esto!!!!!
         String query = "select sp_usuario_insertar(?,?,?,?)";
         //try
-        try (Connection conn = CreateConnection.getInstancia().getConnection(); PreparedStatement ps = conn.prepareStatement(query);
-                ResultSet rs  = ps.executeQuery() ) {
+        try (Connection conn = CreateConnection.getInstancia().getConnection(); PreparedStatement ps = conn.prepareStatement(query); ResultSet rs = ps.executeQuery()) {
             ps.setString(1, user.getUsername());
             ps.setString(2, user.getPassword());
             ps.setString(3, user.getRol());
@@ -25,7 +24,7 @@ public class UsuarioDao {
 
             ps.executeUpdate();
             //parte donde se incerta el id-- se hizo en el procedure
-            if(rs.next()){
+            if (rs.next()) {
                 return rs.getInt(1);
             }
             //usaremos -1 para errores ya que tiene que retorna positivo
@@ -34,54 +33,53 @@ public class UsuarioDao {
             e.printStackTrace();
             return -1;
         }
-      
 
     }
-      //actualizar
+    //actualizar
+
     public boolean actualizar(UsuarioModel user) {
 
         //query
-        String query = "UPDATE usuario set username = ?, password = ?, rol = ?, estado = ? where id = ?";
+        String query = "SELECT sp_usuario_actualizar(?, ?, ?, ?, ?)";
 
         //try
         try (Connection conn = CreateConnection.getInstancia().getConnection(); PreparedStatement ps = conn.prepareStatement(query)) {
 
-            ps.setString(1, user.getUsername());
-            ps.setString(2, user.getPassword());
-            ps.setString(3, user.getRol());
-            ps.setBoolean(4, user.isEstado());
-            ps.setInt(5, user.getId());
+            ps.setInt(1, user.getId());
+            ps.setString(2, user.getUsername());
+            ps.setString(3, user.getPassword());
+            ps.setString(4, user.getRol());
+            ps.setBoolean(5, user.isEstado());
 
-            ps.executeUpdate();
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                return rs.getBoolean(1);
+            }
+            return false;
         } catch (SQLException e) {
             e.printStackTrace();
+            return false;
         }
-        return false;
     }
 
     //consultar
     public List<UsuarioModel> listar() {
         List<UsuarioModel> lista = new ArrayList<>();
-
-        //query
-        String query = "SELECT * FROM usuario ORDER BY id";
-
+        String query = "SELECT * FROM sp_usuario_todos()";
         try (Connection conn = CreateConnection.getInstancia().getConnection(); PreparedStatement ps = conn.prepareStatement(query); ResultSet rs = ps.executeQuery()) {
-            while (rs.next()) {
-                UsuarioModel user = new UsuarioModel(
-                        rs.getInt("id"),
-                        rs.getString("username"),
-                        rs.getString("password"),
-                        rs.getString("rol"),
-                        rs.getBoolean("estado"),
-                        rs.getTimestamp("creado_en").toLocalDateTime());
-                lista.add(user);
-            }
 
+            while (rs.next()) {
+                UsuarioModel u = new UsuarioModel();
+                u.setId(rs.getInt("id"));
+                u.setUsername(rs.getString("username"));
+                u.setPassword(rs.getString("password"));
+                u.setRol(rs.getString("rol"));
+                u.setEstado(rs.getBoolean("estado"));
+                lista.add(u);
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
         return lista;
 
     }
@@ -103,5 +101,29 @@ public class UsuarioDao {
             return false;
         }
 
+    }
+
+    //buscar por username
+    public UsuarioModel buscarPorUsername(String username) {
+        String query = "SELECT * FROM sp_usuario_por_username(?)";
+        try (Connection conn = CreateConnection.getInstancia().getConnection(); PreparedStatement ps = conn.prepareStatement(query)) {
+
+            ps.setString(1, username);
+
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                UsuarioModel user = new UsuarioModel();
+                user.setId(rs.getInt("id"));
+                user.setUsername(rs.getString("username"));
+                user.setPassword(rs.getString("password"));
+                user.setRol(rs.getString("rol"));
+                user.setEstado(rs.getBoolean("estado"));
+                return user;
+            }
+            return null;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
